028d6a4d41e0b44f08bb73db691536814d40316f
hunt <hunt.bao@outlook.com>
2024-09-08 09:54:53 +0800

    npm test success.

---
+++ src/test/math.test.js
@@ -0,0 +1,10 @@
+// math.test.js
+const add = require('./math');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('adds -1 + 1 to equal 0', () => {
+    expect(add(-1, 1)).toBe(0);
+});
