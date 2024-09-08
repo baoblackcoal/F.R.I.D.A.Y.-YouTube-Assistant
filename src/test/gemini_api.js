6521b245ca8786d5623a10248881b9e90b757843
baoblackcoal <baoblackcoal@hotmail.com>
2024-09-08 06:31:03 +0000

    generate llm response from gemini

--- src/test/gemini_api.js
+++ src/test/gemini_api.js
@@ -9,6 +9,7 @@ const gemini_api_key = process.env.GEMINI_API_KEY;
 
 const googleAI = new GoogleGenerativeAI(gemini_api_key);
 
+
 // Set proxy configuration
 const geminiConfig = {
   temperature: 0.9,
@@ -24,15 +25,25 @@ const geminiModel = googleAI.getGenerativeModel({
 
 export const sayHelloByGemini = async () => {
   try {
-    console.log("Say hello to the world.");
     const prompt = "Tell me about google.";
     const result = await geminiModel.generateContent(prompt);
     const response = result.response;
-    console.log(response.text());
   } catch (error) {
     console.log("Response error:", error);
   }
 };
+
+// function input prompt to generate content
+export const generate = async (prompt) => {
+  let text = null
+  try {
+    const result = await geminiModel.generateContent(prompt);
+    text = result.response.text();
+  } catch (error) {
+    console.log("Response error:", error);
+  }
+  return text
+}
  
 // generate();
 
--- src/test/gemini_api.test.js
+++ src/test/gemini_api.test.js
@@ -1,22 +1,22 @@
-import { sayHelloByGemini } from './gemini_api'; // Import the function to test
+import { sayHelloByGemini, generate } from './gemini_api'; // Import the function to test
 
 const axios = require('axios');
 
-// Function to set up axios proxy configuration
-function setupProxy() {
-  const proxyAddress = 'http://192.168.1.16:10811';
-  axios.defaults.proxy = {
-    host: '192.168.1.16',
-    port: 10811
-  };
-}
-
 test('sayHelloByGemini test real api call', (done) => {
-  // Set up proxy before making the call
-  //setupProxy();
-
   sayHelloByGemini().then(() => {
     // Your assertions here
+    done();
+  }).catch(error => {
+    console.error('Error during test:', error);
+    done(error); // Pass the error to done to mark the test as failed
+  });
+}, 20000);
+
+
+test('generate test real api call', (done) => {
+  generate("Hello").then((response_text) => {
+    expect(response_text).not.toBeNull();
+    console.log(response_text);
     done();
   }).catch(error => {
     console.error('Error during test:', error);
