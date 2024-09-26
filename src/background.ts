d1d179bab8ea40ca19b6920f2ddf3cb3e948302e
hunt <hunt.bao@outlook.com>
2024-09-26 18:50:14 +0800

    refactor background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,19 @@
 /// <reference types="chrome"/>
 import { Env, getEnvironment } from './common';
-import { TtsSettings, defaultTtsSettings, SummarySettings, defaultSummarySettings} from './settings';
 import { settingsManager } from "./settingsManager";
 
+// Define interfaces for key components
+interface ITtsSettings {
+    rate: number;
+    pitch: number;
+    volume: number;
+    voiceName: string;
+}
 
+interface ISettingsManager {
+    initializeSettingsWhenInstalled(): Promise<void>;
+    getTtsSettings(): Promise<ITtsSettings>;
+}
 
 // extension first installed
 chrome.runtime.onInstalled.addListener(async () => {
@@ -24,8 +34,6 @@ chrome.runtime.onInstalled.addListener(async () => {
 
 
 let speakTextArray: string[] = [];
-let speakTextSegmentArray: string[] = [];
-let isStartSpeakStream: boolean = false;
 let lastStreamText: string = '';
 let isProcessing: boolean = false;
 let stopStreamSpeakFlag: boolean = false;
@@ -33,76 +41,9 @@ let stopStreamSpeakFlag: boolean = false;
 // Update the speakText function to use settingsManager
 async function speakText(text: string, streamTextIndex: number = 0, playVideo: () => void = () => {}, isStream: boolean = false) {
     if (isStream) {
-        if (stopStreamSpeakFlag) {
-            return;
-        }
-
-        if (text.length == 0) {
-            console.log('(Background)TTS Stream Speaking text is empty');
-            return;
-        }
-        console.log('(Background)TTS Stream Speaking started');
-        lastStreamText += text;
-        console.log(`(Background)TTS Stream Speaking text: ${text}`);
-        console.log(`(Background)TTS Stream Speaking streamTextIndex: ${streamTextIndex}`);
-
-        // if lastStreamText include '\n', then push to speakTextArray
-        if (lastStreamText.includes('\n')) {
-            const textSegments = lastStreamText.split('\n');
-            lastStreamText = textSegments[textSegments.length - 1];
-            for (let i = 0; i < textSegments.length - 1; i++) {
-                speakTextArray.push(textSegments[i]);
-                console.log(`(Background)TTS Stream Speaking speakTextArray0 length: ${speakTextArray.length}`);
-                speakNextText();
-            }
-        }
-
-        //speak next text in speakTextArray in another thread
-        async function speakNextText() {
-            if (isProcessing) {
-                return;
-            }
-            isProcessing = true;
-
-            while (speakTextArray.length > 0) {
-                //wait for tts to finish
-                while (await new Promise(resolve => chrome.tts.isSpeaking(resolve))) {
-                    await new Promise(resolve => setTimeout(resolve, 100));
-                }
-                const settings = await settingsManager.getTtsSettings();
-                const text = speakTextArray.shift();
-                if (text != null) {  
-                    chrome.tts.speak(text, {
-                        rate: settings.rate,
-                        pitch: settings.pitch,
-                        volume: settings.volume,
-                        voiceName: settings.voiceName,
-                        onEvent: (event: chrome.tts.TtsEvent) => {
-                                if (event.type === 'end') {
-                                    if (speakTextArray.length > 0) {
-                                        speakNextText();
-                                        console.log('-------(Background)TTS Stream Speaking tts get end event, but still have text to speak');
-                                    } else {
-                                        console.log('-------(Background)TTS Stream Speaking finished');
-                                        lastStreamText = '';
-                                        isStartSpeakStream = false;
-                                        playVideo();
-                                    }
-                                } else {
-                                    console.log('-------(Background)TTS Stream Speaking tts get event type: ' + event.type);
-                                }
-                            }
-                        });
-
-                }
-            }
-
-            isProcessing = false;
-
-        }
+        handleStreamText(text, streamTextIndex, playVideo);
     } else {
         const settings = await settingsManager.getTtsSettings();
-
         console.log(`(Background)TTS Speaking text: ${text}`);
         chrome.tts.stop();
         chrome.tts.speak(text, {
@@ -120,6 +61,74 @@ async function speakText(text: string, streamTextIndex: number = 0, playVideo: (
     }            
 }
 
+async function handleStreamText(text: string, streamTextIndex: number, playVideo: () => void) {
+    if (stopStreamSpeakFlag) {
+        return;
+    }
+
+    if (text.length == 0) {
+        console.log('(Background)TTS Stream Speaking text is empty');
+        return;
+    }
+    console.log('(Background)TTS Stream Speaking started');
+    lastStreamText += text;
+    console.log(`(Background)TTS Stream Speaking text: ${text}`);
+    console.log(`(Background)TTS Stream Speaking streamTextIndex: ${streamTextIndex}`);
+
+    // if lastStreamText include '\n', then push to speakTextArray
+    if (lastStreamText.includes('\n')) {
+        const textSegments = lastStreamText.split('\n');
+        lastStreamText = textSegments[textSegments.length - 1];
+        for (let i = 0; i < textSegments.length - 1; i++) {
+            speakTextArray.push(textSegments[i]);
+            console.log(`(Background)TTS Stream Speaking speakTextArray0 length: ${speakTextArray.length}`);
+            speakNextText();
+        }
+    }
+
+    //speak next text in speakTextArray in another thread
+    async function speakNextText() {
+        if (isProcessing) {
+            return;
+        }
+        isProcessing = true;
+
+        while (speakTextArray.length > 0) {
+            //wait for tts to finish
+            while (await new Promise(resolve => chrome.tts.isSpeaking(resolve))) {
+                await new Promise(resolve => setTimeout(resolve, 100));
+            }
+            const settings = await settingsManager.getTtsSettings();
+            const text = speakTextArray.shift();
+            if (text != null) {  
+                chrome.tts.speak(text, {
+                    rate: settings.rate,
+                    pitch: settings.pitch,
+                    volume: settings.volume,
+                    voiceName: settings.voiceName,
+                    onEvent: (event: chrome.tts.TtsEvent) => {
+                            if (event.type === 'end') {
+                                if (speakTextArray.length > 0) {
+                                    speakNextText();
+                                    console.log('-------(Background)TTS Stream Speaking tts get end event, but still have text to speak');
+                                } else {
+                                    console.log('-------(Background)TTS Stream Speaking finished');
+                                    lastStreamText = '';
+                                    playVideo();
+                                }
+                            } else {
+                                console.log('-------(Background)TTS Stream Speaking tts get event type: ' + event.type);
+                            }
+                        }
+                    });
+
+            }
+        }
+
+        isProcessing = false;
+
+    }
+}
 
 chrome.contextMenus.onClicked.addListener((info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
     if (!tab) return; // Handle case when tab is undefined
@@ -131,43 +140,42 @@ chrome.contextMenus.onClicked.addListener((info: chrome.contextMenus.OnClickData
     }
 });
 
-// Listen for messages from popup.js
 chrome.runtime.onMessage.addListener((message: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) => {
-    if (message.action === 'resetWhenPageChange') {
-        speakTextArray = [];
-        stopStreamSpeakFlag = false;
-    } else if (message.action === 'speak') {
-        stopStreamSpeakFlag = false;
-        const text = message.text;
-        const isStream = message.isStream;
-        speakText(text);
-    } else if (message.action === 'speakAndPlayVideo') {
-        const text = message.text;
-        const isStream = message.isStream || false;
-        const streamTextIndex = message.streamTextIndex || 0;
-        speakText(text, streamTextIndex, () => {
-            // Send message to content script to play the video
-            if (sender.tab && sender.tab.id !== undefined) {
-                chrome.tabs.sendMessage(sender.tab.id, { action: 'playVideo' });
+    switch (message.action) {
+        case 'resetWhenPageChange':
+            speakTextArray = [];
+            stopStreamSpeakFlag = false;
+            break;
+        case 'speak':
+            stopStreamSpeakFlag = false;
+            speakText(message.text);
+            break;
+        case 'speakAndPlayVideo':
+            speakText(message.text, message.streamTextIndex || 0, () => {
+                if (sender.tab && sender.tab.id !== undefined) {
+                    chrome.tabs.sendMessage(sender.tab.id, { action: 'playVideo' });
+                }
+            }, message.isStream || false);
+            sendResponse({ status: "success" });
+            break;
+        case 'ttsStop':
+            stopStreamSpeakFlag = true;
+            speakTextArray = [];
+            chrome.tts.stop();
+            break;
+        case 'ttsCheckSpeaking':
+            chrome.tts.isSpeaking((isSpeaking) => {
+                sendResponse({ isSpeaking });
+            });
+            return true; // Indicates that the response will be sent asynchronously
+        case 'openOptionsPage':
+            if (chrome.runtime.openOptionsPage) {
+                chrome.runtime.openOptionsPage();
+            } else {
+                window.open(chrome.runtime.getURL('options.html'));
             }
-        }, isStream);
-        sendResponse({status: "success"});
-    } else if (message.action === 'ttsStop') {
-        stopStreamSpeakFlag = true;
-        speakTextArray = [];//clear speakTextArray for next speaking
-        chrome.tts.stop();
-    } else if (message.action === 'ttsCheckSpeaking') {
-        chrome.tts.isSpeaking((isSpeaking) => {
-            sendResponse({ isSpeaking });
-        });
-        return true; // Indicates that the response will be sent asynchronously
-    } else if (message.action === 'openOptionsPage') {
-        if (chrome.runtime.openOptionsPage) {
-            chrome.runtime.openOptionsPage();
-        } else {
-            window.open(chrome.runtime.getURL('options.html'));
-        }
-    } else {
-        console.log(`(Background)Unknown message action: ${message.action}`);
+            break;
+        default:
+            console.log(`(Background)Unknown message action: ${message.action}`);
     }
 });
