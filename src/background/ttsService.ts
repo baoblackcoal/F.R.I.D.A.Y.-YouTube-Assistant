e9e8a2f074583d4d0e57ba851ec09edc913b3b7b
hunt <hunt.bao@outlook.com>
2024-10-06 07:44:23 +0800

    rafator ttsServices.ts

--- src/background/ttsService.ts
+++ src/background/ttsService.ts
@@ -37,16 +37,12 @@ export class TtsService implements ITtsService {
     }
 
     async handleStreamText(text: string, index: number, sender: chrome.runtime.MessageSender = this.defaultSender, playVideo: () => void = () => { }): Promise<void> {
-        if (this.stopStreamSpeakFlag) {
-            return;
-        }
-
-        if (text.length == 0) {
+        if (this.stopStreamSpeakFlag || text.length === 0) {
             return;
         }
 
         this.ttsSettings = await this.settingsManager.getTtsSettings();
-        this.speakTextArray.push({ text: text, index: index });
+        this.speakTextArray.push({ text, index });
         this.speakNextText(false, sender, playVideo);
     }
 
@@ -61,65 +57,68 @@ export class TtsService implements ITtsService {
         this.isProcessing = true;
 
         while (this.speakTextArray.length > 0 || this.speakingText.length > 0) {
-            if (this.speakingText.length > 0 || this.speakingText == 'start_speak_flag') {
-                let text = '';
-                if (this.speakingText == 'start_speak_flag') {
-                    this.speakingText = '';
-                    text = ' '; //for tts speak finish callback
-                }
-                if (this.speakingText.length > 0) {
-                    text = this.speakingText;
-                    this.speakingText = '';
-                }
+            if (this.speakingText.length > 0 || this.speakingText === 'start_speak_flag') {
+                let text = this.speakingText === 'start_speak_flag' ? ' ' : this.speakingText;
+                this.speakingText = '';
+
                 chrome.tts.speak(text, {
                     rate: this.ttsSettings.rate,
                     pitch: this.ttsSettings.pitch,
                     volume: this.ttsSettings.volume,
                     voiceName: this.ttsSettings.voiceName,
-                    onEvent: (event: chrome.tts.TtsEvent) => {
-                        // console.log("event.type : ", event.type);
-                        if (event.type === 'end') {
-                            if (this.speakTextArray.length > 0) {
-                                //get next text, but check if it's not empty
-                                let getNextText = false;
-                                let index = 0;
-                                while (true) {
-                                    const nextText = this.speakTextArray.shift();
-                                    this.speakingText = nextText?.text || '';
-                                    index = nextText?.index || 0;
-                                    if (this.speakingText.length > 0) {
-                                        getNextText = true;
-                                        break;
-                                    } else if (this.speakTextArray.length == 0) {
-                                        break;
-                                    }
-                                }
-                                if (getNextText) {
-                                    console.log("speakNextText: ", this.speakingText);
-                                    //sent current text to content-script
-                                    if (sender.tab && sender.tab.id !== undefined) {
-                                        chrome.tabs.sendMessage(sender.tab.id, { action: 'ttsSpeakingText', index: index });
-                                        chrome.tabs.sendMessage(sender.tab.id, { action: 'ttsEnableAccpetMessage', index: index });
-                                    }
-                                    this.speakNextText(true, sender, playVideo);
-                                }
-                            } else {
-                                this.speakingText = '';
-                                this.lastStreamText = '';
-                                this.isProcessing = false;
-                                this.speakingText = 'start_speak_flag';
-                                playVideo();
-                            }
-                        } else {
-                        }
-                    }
+                    onEvent: (event: chrome.tts.TtsEvent) => this.handleTtsEvent(event, sender, playVideo)
                 });
             }
-            //wait tts speak finish
+
             while (await new Promise(resolve => chrome.tts.isSpeaking(resolve))) {
                 await new Promise(resolve => setTimeout(resolve, 100));
             }
         }
+    }
+
+    private handleTtsEvent(event: chrome.tts.TtsEvent, sender: chrome.runtime.MessageSender, playVideo: () => void): void {
+        if (event.type === 'end') {
+            if (this.speakTextArray.length > 0) {
+                this.processNextText(sender, playVideo);
+            } else {
+                this.resetSpeakingState(playVideo);
+            }
+        }
+    }
+
+    private processNextText(sender: chrome.runtime.MessageSender, playVideo: () => void): void {
+        let getNextText = false;
+        let index = 0;
+
+        while (true) {
+            const nextText = this.speakTextArray.shift();
+            this.speakingText = nextText?.text || '';
+            index = nextText?.index || 0;
+
+            if (this.speakingText.length > 0) {
+                getNextText = true;
+                break;
+            } else if (this.speakTextArray.length === 0) {
+                break;
+            }
+        }
+
+        if (getNextText) {
+            console.log("speakNextText: ", this.speakingText);
+            if (sender.tab && sender.tab.id !== undefined) {
+                chrome.tabs.sendMessage(sender.tab.id, { action: 'ttsSpeakingText', index });
+                chrome.tabs.sendMessage(sender.tab.id, { action: 'ttsEnableAccpetMessage', index });
+            }
+            this.speakNextText(true, sender, playVideo);
+        }
+    }
+
+    private resetSpeakingState(playVideo: () => void): void {
+        this.speakingText = '';
+        this.lastStreamText = '';
+        this.isProcessing = false;
+        this.speakingText = 'start_speak_flag';
+        playVideo();
     }
 
     stopStreamSpeak() {
