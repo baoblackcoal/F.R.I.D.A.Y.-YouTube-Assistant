86ed648f3184cb260bec4a1c0fce9e6cdfbca469
hunt <hunt.bao@outlook.com>
2025-01-11 07:37:41 +0800

    small change

--- src/contentscript/index.ts
+++ src/contentscript/index.ts
@@ -28,38 +28,38 @@ window.onload = async () => {
         }
     }
 
-    if (window.location.hostname === "chat.openai.com") {
-        const textarea = document.getElementsByTagName("textarea")[0];
-        if (textarea) {
-            textarea.focus();
-            if (window.location.search === "?ref=glasp") {
-                chrome.runtime.sendMessage({ message: "getPrompt" }, (response: { prompt: string }) => {
-                    textarea.value = response.prompt;
-                    if (response.prompt !== "") {
-                        textarea.focus();
-                        const buttons = document.getElementsByTagName("button");
-                        buttons[buttons.length - 1].click();
-                    }
-                });
-            }
-        }
-    }
+    // if (window.location.hostname === "chat.openai.com") {
+    //     const textarea = document.getElementsByTagName("textarea")[0];
+    //     if (textarea) {
+    //         textarea.focus();
+    //         if (window.location.search === "?ref=glasp") {
+    //             chrome.runtime.sendMessage({ message: "getPrompt" }, (response: { prompt: string }) => {
+    //                 textarea.value = response.prompt;
+    //                 if (response.prompt !== "") {
+    //                     textarea.focus();
+    //                     const buttons = document.getElementsByTagName("button");
+    //                     buttons[buttons.length - 1].click();
+    //                 }
+    //             });
+    //         }
+    //     }
+    // }
 
-    if (window.location.hostname === "example.com") {
-        document.body.style.border = "5px solid red";
+    // if (window.location.hostname === "example.com") {
+    //     document.body.style.border = "5px solid red";
 
-        // Send a message to the background worker
-        chrome.runtime.sendMessage({ action: 'fetchData' }, (response) => {
-            if (!response) {
-                console.error('No response received from background script');
-                return;
-            } else if (response.error) {
-                console.log(response.error);
-            } else {
-                console.log('Data received from background:', response.data);
-            }
-        });
-    }
+    //     // Send a message to the background worker
+    //     chrome.runtime.sendMessage({ action: 'fetchData' }, (response) => {
+    //         if (!response) {
+    //             console.error('No response received from background script');
+    //             return;
+    //         } else if (response.error) {
+    //             console.log(response.error);
+    //         } else {
+    //             console.log('Data received from background:', response.data);
+    //         }
+    //     });
+    // }
 
     // chrome.runtime.sendMessage({ action: 'getSettings' }, (response: { setting: any }) => {
     //     const setting = response.setting || {};
